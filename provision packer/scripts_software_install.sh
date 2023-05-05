c548b6321248ce936d4b7c8871e16790bd3eacf8
Jimsichu <97720745+Jimsichu@users.noreply.github.com>
2023-05-05 08:43:18 +0800

    Update scripts_software_install.sh
    include install vmware

--- provision packer/scripts_software_install.sh
+++ provision packer/scripts_software_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+sudo apt-get update
 # Install PyCharm
 sudo snap install pycharm-community --classic #pycharm community ver
 sudo snap install pycharm-professional --classic #pycharm professional ver
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+wget https://download3.vmware.com/software/wkst/file/VMware-Workstation-Full-16.2.1-18811642.x86_64.bundle
+chmod +x VMware-Workstation-Full-16.2.1-18811642.x86_64.bundle
+sudo ./VMware-Workstation-Full-16.2.1-18811642.x86_64.bundle
